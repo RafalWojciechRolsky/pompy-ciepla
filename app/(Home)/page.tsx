f43f3b8c6d0cf9edbcc05f322d965aaca91eccf2
RafalWojciechRolsky <raaf.em@gmail.com>
2023-09-30 22:49:20 +0200

    question component

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,13 +1,42 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
 import Hero from '@/components/Hero';
 import Logos from '@/components/Logos';
 import Question from '@/components/Question';
 
-export default function Home() {
+interface QuestionType {
+  id: number;
+  title: string;
+  body: string[];
+  links: string[];
+  textOnLeft: boolean;
+  backgroundColor: string;
+  imageUrl: string;
+}
+
+const HomePage = async () => {
+  const jsonDir = path.join(process.cwd(), 'data', '');
+  const data = await fs.readFile(path.join(jsonDir, 'questions.json'), 'utf8');
+  const questions: QuestionType[] = JSON.parse(data);
+
   return (
     <>
       <Hero />
       <Logos />
-      <Question />
+      {questions.map((question) => (
+        <Question
+          key={question.id}
+          title={question.title}
+          body={question.body}
+          links={question.links}
+          textOnLeft={question.textOnLeft}
+          backgroundColor={question.backgroundColor}
+          imageUrl={question.imageUrl}
+        />
+      ))}
     </>
   );
-}
+};
+
+export default HomePage;
--- components/Question.tsx
+++ components/Question.tsx
@@ -2,24 +2,38 @@ import Image from 'next/image';
 import { FC } from 'react';
 
 interface IPropsTypes {
-  name?: string;
+  title: string;
+  body: string[];
+  links: string[];
+  textOnLeft: boolean;
+  backgroundColor: string;
+  imageUrl: string;
 }
 
 const Question: FC<IPropsTypes> = (props) => {
   return (
-    <div className="question">
-      <section className="question__container">
+    <div className="question" style={{ backgroundColor: props.backgroundColor }}>
+      <section className={`question__container ${props.textOnLeft ? 'row' : 'row-reverse'}`}>
         <div>
-          <h2>Czy zakup pompy ciepła to dobra decyzja?</h2>
-          <p>
-            Pomaga ona zmniejszyć roczną emisję dwutlenku węgla o 44%, a w całym okresie użytkowania
-            kosztuje nie więcej niż kocioł gazowy. Urządzenia te są skuteczne, niezawodne i
-            przyszłościowe - w przeciwieństwie do kotłów gazowych.
-          </p>
+          <h2>{props.title}</h2>
+          {props.body.length === 1 ? (
+            <p>{props.body[0]}</p>
+          ) : (
+            <ul>
+              {props.body.map((item) => {
+                return <li key={item}>{item}</li>;
+              })}
+            </ul>
+          )}
         </div>
         <div className="question__imageContainer">
           <figure>
-            <Image alt="Question Image" src="/images/Image-101.jpg" width={400} height={400} />
+            <Image
+              alt="Question Image"
+              src={`/images/${props.imageUrl}`}
+              width={400}
+              height={400}
+            />
           </figure>
         </div>
       </section>

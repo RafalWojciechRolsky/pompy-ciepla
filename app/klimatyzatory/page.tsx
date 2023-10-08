254df8c0923948ca1a7354008033c7f9cd84ef02
RafalWojciechRolsky <raaf.em@gmail.com>
2023-10-08 23:08:28 +0200

    klimatyzatory components

--- app/klimatyzatory/page.tsx
+++ app/klimatyzatory/page.tsx
@@ -20,6 +20,111 @@ const KlimatyzatoryPage = async () => {
     <>
       <Hero title={hero[4].title} points={hero[4].points} imageUrl={hero[4].imageUrl} />
       <Logos />
+      <section className="airConditioners">
+        <div className="airConditioners__container">
+          <h2>Czym są i jak działają klimatyzatory</h2>
+          <p>
+            Klimatyzacja stała się nieodłącznym elementem naszego życia, zapewniając komfort
+            termiczny w gorące dni i nie tylko. Ale czy kiedykolwiek zastanawiałeś się, jak
+            dokładnie działają te urządzenia i jakie są różnice między klimatyzatorami a pompami
+            ciepła? W tym wpisie przybliżymy budowę, działanie oraz rodzaje klimatyzatorów, a także
+            wyjaśnimy, w jaki sposób nowoczesne technologie, takie jak klimatyzatory inwerterowe,
+            łączą w sobie funkcje chłodzenia i ogrzewania. Jeśli chcesz dowiedzieć się więcej o
+            tajnikach klimatyzacji i wyborze odpowiedniego urządzenia do Twojego domu czy biura,
+            koniecznie przeczytaj dalej!
+          </p>
+          <h3>Budowa i działanie klimatyzatora</h3>
+          <p>Podstawowe elementy klimatyzatora to:</p>
+          <ul>
+            <li>sprężarka </li>
+            <li>parownik </li>
+            <li>skraplacz</li>
+            <li>wentylator</li>
+          </ul>
+          <p>
+            W parowniku następuje odparowanie czynnika chłodniczego, który pochłania ciepło z
+            pomieszczenia. Następnie czynnik ten jest sprężany w sprężarce, co powoduje wzrost jego
+            temperatury. Sprężony czynnik jest przetłaczany do skraplacza, który znajduje się na
+            zewnątrz budynku. W skraplaczu ciepło z czynnika jest oddawane do powietrza
+            zewnętrznego, a czynnik skrapla się i staje cieczą. Ciecz dostaje się do elementu
+            rozprężnego, gdzie jest dławiona, co powoduje zmniejszenie jej ciśnienia i temperatury.
+            Schłodzony czynnik w postaci cieczy ponownie zostaje podany na parownik, gdzie się
+            ogrzewa od powietrza w pomieszczeniu i przechodzi w stan gazowy.
+          </p>
+          <h3>Rodzaje klimatyzatorów</h3>
+          <p>
+            Klimatyzatory dzielą się na różne typy, w zależności od budowy i rozwiązań montażowych.
+            Najpopularniejsze są klimatyzatory typu split, które składają się z dwóch jednostek –
+            wewnętrznej i zewnętrznej – połączonych ze sobą linią chłodniczą. W obrębie
+            klimatyzatorów typu split i multi-split można wyróżnić następujące rodzaje klimatyzacji:
+          </p>
+          <ul>
+            <li>klimatyzatory ścienne</li>
+            <li>klimatyzatory ścienno-przysufitowe</li>
+            <li>klimatyzatory przypodłogowe, klimatyzatory</li>
+            <li>kasetonowe, klimatyzatory kanałowe</li>
+          </ul>
+          <p>
+            Wybór konkretnego rodzaju klimatyzacji zależy od indywidualnych preferencji oraz efektu
+            wizualnego, jaki chcemy osiągnąć.
+          </p>
+          <h4>Klimatyzatory multi-split</h4>
+          <p>
+            Klimatyzatory typu multi-split to urządzenia klimatyzacyjne zbudowane tak samo jak
+            klimatyzatory typu split, ale do jednej jednostki zewnętrznej podłącza się wiele
+            jednostek wewnętrznych. Pozwala to na schładzanie powietrza w kilku pomieszczeniach
+            jednocześnie z pomocą jednej instalacji.
+          </p>
+          <h4>Klimatyzatory przenośne</h4>
+          <p>
+            Klimatyzatory przenośne to urządzenia, które mają wszystkie podzespoły (parownik,
+            skraplacz, sprężarka, zawór rozprężny i wentylator) w jednej obudowie. Są one łatwe do
+            przenoszenia pomiędzy różnymi pomieszczeniami, ale generują większy hałas w czasie pracy
+            i mają niższą wydajność niż klimatyzatory stacjonarne.
+          </p>
+          <h4>Klimatyzatory jako pompy ciepła</h4>
+          <p>
+            Warto zauważyć, że klimatyzatory mogą również działać jako pompy ciepła, dzięki
+            odwróceniu układu chłodniczego. W takim przypadku parownik staje się skraplaczem, co
+            pozwala na ogrzewanie pomieszczenia.
+          </p>
+          <h3>Czyli klimatyzatory to to samo co pompy ciepła?</h3>
+          <p>
+            Klimatyzatory i pompy ciepła to nie są tożsame urządzenia, choć mają wiele wspólnych
+            cech i mogą pełnić podobne funkcje. Obie te technologie wykorzystują procesy
+            termodynamiczne do przenoszenia ciepła, ale różnią się w sposobie, w jaki to robią i w
+            zakresie ich zastosowań.
+          </p>
+          <h4>Różnice między klimatyzatorami a pompami ciepła</h4>
+          <p>
+            Klimatyzator to urządzenie, które służy głównie do chłodzenia pomieszczeń poprzez
+            usuwanie ciepła z powietrza wewnętrznego i odprowadzanie go na zewnątrz. Klimatyzatory
+            mogą również poprawiać jakość powietrza poprzez filtrację oraz usuwanie bakterii i
+            grzybów. Klimatyzatory rozprowadzają chłód po pomieszczeniu bezpośrednio nadmuchowo.
+          </p>
+          <p>
+            Z drugiej strony, pompa ciepła to urządzenie, które może służyć do chłodzenia i
+            ogrzewania pomieszczeń poprzez wykorzystanie ciepła z zewnętrznych źródeł, takich jak
+            powietrze, woda czy gleba. Pompy ciepła są bardziej wszechstronne niż klimatyzatory,
+            ponieważ mogą być wykorzystywane nie tylko do chłodzenia, ale także do ogrzewania
+            pomieszczeń oraz przygotowania ciepłej wody użytkowej. Pompy ciepła rozprowadzają chłód
+            poprzez czynnik pośredni, czyli wodę krążącą w instalacji.
+          </p>
+          <h4>Nowoczesne klimatyzatory inwerterowe</h4>
+          <p>
+            Warto zauważyć, że niektóre nowoczesne klimatyzatory, zwane klimatyzatorami
+            inwerterowymi, wykorzystują technologię pomp ciepła do chłodzenia i ogrzewania
+            pomieszczeń.
+          </p>
+          <h4>Podsumowanie</h4>
+          <p>
+            Podsumowując, klimatyzatory i pompy ciepła to różne urządzenia, które mogą pełnić
+            podobne funkcje, ale różnią się w sposobie, w jaki to robią i w zakresie ich zastosowań.
+            Wybór między nimi zależy od indywidualnych potrzeb i preferencji, a także od specyfiki
+            budynku, w którym mają być zainstalowane.
+          </p>
+        </div>
+      </section>
     </>
   );
 };
--- components/Movies.tsx
+++ components/Movies.tsx
@@ -1,24 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
-import { FC } from 'react';
-import YouTube, { YouTubeProps } from 'react-youtube';
+import { FC, useState } from 'react';
+// import YouTube, { YouTubeProps } from 'react-youtube';
+import ReactPlayer from 'react-player';
 
 interface IPropsTypes {
   name?: string;
 }
 
 const Movies: FC<IPropsTypes> = (props) => {
-  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    event.target.pauseVideo();
-  };
+  const [play, setPlay] = useState(false);
+  // const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+  //   event.target.pauseVideo();
+  // };
 
-  const opts: YouTubeProps['opts'] = {
-    // height: '500',
-    // width: '889',
-    playerVars: {
-      autoplay: 0,
-    },
-  };
+  // const opts: YouTubeProps['opts'] = {
+  //   // height: '500',
+  //   // width: '889',
+  //   playerVars: {
+  //     autoplay: 0,
+  //   },
+  // };
 
   return (
     <div className="movies">
@@ -30,7 +33,18 @@ const Movies: FC<IPropsTypes> = (props) => {
           jakie są ich niewątpliwe zalety.
         </p>
         <div className="videoContainer">
-          <YouTube videoId="chiXIU5FjlE" opts={opts} onReady={onPlayerReady} />;
+          {/* <YouTube videoId="chiXIU5FjlE" opts={opts} onReady={onPlayerReady} />; */}
+          {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+          <div onClick={() => setPlay(true)}>
+            {play ? (
+              <ReactPlayer url="https://www.youtube.com/watch?v=oDAknaJ9Kpo" playing />
+            ) : (
+              <img
+                src="https://img.youtube.com/vi/oDAknaJ9Kpo/maxresdefault.jpg"
+                alt="Video thumbnail"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>

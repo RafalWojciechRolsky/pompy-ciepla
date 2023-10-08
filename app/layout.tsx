9e6185514114626e57359a2db99722777e5e3fd3
RafalWojciechRolsky <raaf.em@gmail.com>
2023-10-08 09:57:31 +0200

    testimonials component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Ubuntu } from 'next/font/google';
 import Footer from '@/components/Footer';
 import Sidebar from '@/components/Header/Sidebar';
 import Contact from '@/components/FormContact/Contact';
+import Testimonials from '@/components/Testimonials';
 
 const ubuntu = Ubuntu({ weight: ['300', '400', '500', '700'], subsets: ['latin-ext'] });
 
@@ -20,11 +21,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://justreview.co/widget/justreview.css" />
+      </head>
       <body className={ubuntu.className} id="outer-container">
         <Sidebar />
         <div id="page-wrap">
           <Header />
           <main>{children}</main>
+          <Testimonials />
           <Contact />
           <Footer />
         </div>
